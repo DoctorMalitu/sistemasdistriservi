0fcb0b36d950688ea3ed8ab86d142fc5d6a8bf8b
DoctorMalitu <josemora96@hotmail.com>
2020-10-28 02:50:45 -0500

    Subgfgfgf

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', ${DATABASE_HOST}),
-        port: env.int('DATABASE_PORT', ${DATABASE_PORT}),
-        database: env('DATABASE_NAME', ${DATABASE_NAME}),
-        username: env('DATABASE_USERNAME', ${DATABASE_USERNAME}),
-        password: env('DATABASE_PASSWORD', ${DATABASE_PASSWORD}),
+        host: env('DATABASE_HOST', 'ec2-54-224-175-142.compute-1.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'd82ocbopaf59u5'),
+        username: env('DATABASE_USERNAME', 'zsqhlbjvcklihi'),
+        password: env('DATABASE_PASSWORD', 'c9ee4e126f9d58c57ae8ec232250debb28b31f9070e464ab0987ff6a3ae2e28c'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
